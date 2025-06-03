8471fac8f832bb136699199a15d8deaf4cabd289
vikas vishwakarma <codewithvikas0@gmail.com>
2025-06-03 12:48:44 +0530

    Update node.js

--- node.js
+++ node.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: '/tmp/' });
 
 // Serve static files
 app.use(express.static('public'));
